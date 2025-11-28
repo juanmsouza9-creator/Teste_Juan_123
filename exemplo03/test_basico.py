b60ca172bd77797f706a8085db5b6d365b3bef02
Juan Macos de Souza <juan.m.souza9@aluno.senai.br>
2025-11-28 11:19:32 +0000

    arrruamndo exemplo3

--- exemplo03/test_basico.py
+++ exemplo03/test_basico.py
@@ -1,12 +1,11 @@
 import pytest
 
-@pytest.fixture
+@pytest.fixture ## ele é o decorador em python ##
 def sample_list():
-    return [1,2,3,4,5]
+    return[1,2,3,4,5]
 
 def test_sum(sample_list):
     assert sum(sample_list) == 15
 
-def test_length(sample_list):
+def test_lenth(sample_list):
     assert len(sample_list) == 5
-
